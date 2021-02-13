26311562a9d5e68828f3b167cfaa010d9abd19c8
mac Elbadri <fa_lebdiri@esi.dz>
2021-02-13 04:23:47 +0100

    Initial commit

---
+++ lib/jitsi_meeting_listener.dart
@@ -0,0 +1,17 @@
+/// Class holding the callback functions for conference events
+class JitsiMeetingListener {
+  final Function({Map<dynamic, dynamic> message}) onConferenceWillJoin;
+  final Function({Map<dynamic, dynamic> message}) onConferenceJoined;
+  final Function({Map<dynamic, dynamic> message}) onConferenceTerminated;
+  final Function({Map<dynamic, dynamic> message}) onPictureInPictureWillEnter;
+  final Function({Map<dynamic, dynamic> message}) onPictureInPictureTerminated;
+  final Function(dynamic error) onError;
+
+  JitsiMeetingListener(
+      {this.onConferenceWillJoin,
+      this.onConferenceJoined,
+      this.onConferenceTerminated,
+      this.onPictureInPictureWillEnter,
+      this.onPictureInPictureTerminated,
+      this.onError});
+}
